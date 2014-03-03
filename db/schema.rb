c72dfd6a111f0892ee2d5952acf670a28ed0e85c
Zane <pzwoodfin@gmail.com>
2014-03-03 17:44:49 -0500

    Removes home content.

--- db/schema.rb
+++ db/schema.rb
@@ -13,6 +13,9 @@
 
 ActiveRecord::Schema.define(version: 20140211200420) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "scores", force: true do |t|
     t.string   "name"
     t.integer  "total"
